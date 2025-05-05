1ec2fb8f3aac37d55ee1c6cd22e0aaab2cd6b6dd
RachanaB5 <rachanabhaskargowda05@gmail.com>
2025-05-05 09:48:04 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,6 @@
 # backend.py
 from flask import Flask, request, jsonify
-from queue_logic import TriageQueue, Patient
+from backend.queue_logic import TriageQueue, Patient
 from flask_cors import CORS
 
 app = Flask(__name__)
